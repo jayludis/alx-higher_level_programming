892a7bb1dca7590a9648b503c59c306904efb5d4
jayludis <jayludis@gmail.com>
2024-09-10 20:39:44 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+9-print_last_digit.py#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_digit = number % -(10)
+        print(-(last_digit), end='')
+    else:
+        last_digit = number % 10
+        print(last_digit, end='')
+    return abs(last_digit)
+
